0e2c9dbffcad0b7d322e10aadde14ad2052ff813
phaustin0 <zechy14@icloud.com>
2021-07-09 18:22:31 +0800

    added responsive testimonialcard component for achievements section

---
+++ src/components/TestimonialCard/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container, Content, Author } from "./TestimonialCard";
+
+const TestimonialCard = ({ content, author }) => {
+  return (
+    <Container>
+      <Content>{content}</Content>
+      <Author>{author}</Author>
+    </Container>
+  );
+};
+
+export default TestimonialCard;
