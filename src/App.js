388a7fae6904ca56fd95250414f6c7882f8ff204
phaustin0 <zechy14@icloud.com>
2021-07-09 14:26:42 +0800

    remove @import statement from globalstyles variable

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import About from "components/About";
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-  @import url("https://fonts.googleapis.com/css2?family=Galada&family=Montserrat:wght@400;500&family=Mrs+Sheppards&family=Overpass:wght@400;600;700&family=Poppins:wght@400;700&display=swap");
-
   * {
     padding: 0;
     margin: 0;
